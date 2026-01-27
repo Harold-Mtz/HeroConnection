5015c4f2185ed70bcea7b8c07482ce20d826ce3a
Harold Hernandez <martinezharold059@gmail.com>
2026-01-27 14:18:44 -0600

    feat: add ApiResponse trait

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    protected function successResponse($data, $message = null, $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+            'error' => null,
+            'ip' => request()->ip_address
+        ], $code);
+    }
+
+    protected function errorResponse($message, $error, $code): JsonResponse
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'data' => null,
+            'error' => $error
+        ], $code);
+    }
+}
